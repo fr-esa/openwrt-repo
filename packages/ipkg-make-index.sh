06b7ff62c0b5e7f3b77ab13d1a31994e350ab2c9
fr-esa <arichikaari.me@gmail.com>
2025-11-02 22:05:06 +0300

    Update ipkg-make-index.sh

--- packages/ipkg-make-index.sh
+++ packages/ipkg-make-index.sh
@@ -4,32 +4,42 @@ set -e
 pkg_dir=$1
 
 if [ -z "$pkg_dir" ] || [ ! -d "$pkg_dir" ]; then
-    echo "Usage: ipkg-make-index <package_directory>" >&2
-    exit 1
+  echo "Usage: ipkg-make-index <package_directory>" >&2
+  exit 1
 fi
 
 empty=1
 
-for pkg in $(find "$pkg_dir" -name '*.ipk' | sort); do
-    empty=
-    name="${pkg##*/}"
-    name="${name%%_*}"
-    [[ "$name" == "kernel" ]] && continue
-    [[ "$name" == "libc" ]] && continue
-    echo "Generating index for package $pkg" >&2
-
-    file_size=$(stat -L -c%s "$pkg")
-    sha256sum=$(sha256sum "$pkg" | cut -d ' ' -f1)
-
-    sed_safe_pkg=$(echo "$pkg" | sed 's/^\.\///; s/\//\\\//g')
-
-    # Извлечь control.tar.gz из ar-архива, затем из control.tar.gz извлечь файл control
-    # Используем 'ar p' чтобы вывести control.tar.gz на stdout, потом распаковать и получить control
-    ar p "$pkg" control.tar.gz | tar -Oxzf - control | \
-    sed -e "s|^Description:|Filename: $sed_safe_pkg\nSize: $file_size\nSHA256sum: $sha256sum\nDescription:|"
-
-    echo ""
+for pkg in "$pkg_dir"/*.ipk; do
+  [ -e "$pkg" ] || continue
+  empty=
+  echo "Generating index for package $pkg" >&2
+  
+  tempdir=$(mktemp -d)
+  
+  # Распаковываем .ipk как gzip-архив (внутри должны быть control.tar.gz и data.tar.gz)
+  tar -xzf "$pkg" -C "$tempdir"
+  
+  if [ ! -f "$tempdir/control.tar.gz" ]; then
+    echo "No control.tar.gz found in $pkg" >&2
+    rm -rf "$tempdir"
+    continue
+  fi
+  
+  file_size=$(stat -L -c%s "$pkg")
+  sha256sum=$(sha256sum "$pkg" | cut -d' ' -f1)
+  
+  # Извлекаем файл control из control.tar.gz и заменяем Description, добавляем метаданные
+  tar -Oxzf "$tempdir/control.tar.gz" ./control | sed -e "s/^Description:/Filename: ${pkg##*/}\
+Size: $file_size\
+SHA256sum: $sha256sum\
+Description:/"
+  
+  echo
+  
+  rm -rf "$tempdir"
 done
 
-[ -n "$empty" ] && echo
+[ -n "$empty" ] && echo >&2 "No packages found in $pkg_dir"
+
 exit 0
